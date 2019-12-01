49f4deeea64ae9c24649b5d3afd5a7c089a39fb5
fwcd <fwcdmail@gmail.com>
2019-12-01 23:55:29 +0100

    Implement part 2 of day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,5 +1,16 @@
+-- Part 1
+
 fuel :: Int -> Int
 fuel m = (m `div` 3) - 2
 
 totalFuel :: [Int] -> Int
 totalFuel = foldr (+) 0 . map fuel
+
+-- Part 2
+fuelCumulative :: Int -> Int
+fuelCumulative m | m > 0 = max 0 $ f + fuelCumulative f
+                 | otherwise = 0
+    where f = fuel m
+
+totalFuelCumulative :: [Int] -> Int
+totalFuelCumulative = foldr (+) 0 . map fuelCumulative
