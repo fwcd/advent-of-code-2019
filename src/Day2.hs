37f1e7d09949556a6a80819a89aea4e47999e5cf
fwcd <fwcdmail@gmail.com>
2019-12-02 11:59:10 +0100

    Implement input finder (day 2, part 2)
    Brute-force search for the inputs that produce a given output in the
    program.

--- src/Day2.hs
+++ src/Day2.hs
@@ -19,3 +19,19 @@ interpret m = interpret' m m
           interpret' (99:_) = id
           interpret' (op:x:y:z:p) = interpret' p . performOp op x y z
           interpret' p = const $ error $ "Parse error on remaining program: " <> show p
+
+-- Interprets a program written in Intcode with inputs and returns the value in the first cell.
+interpretWithInputs :: Int -> Int -> [Int] -> Int
+interpretWithInputs x y (op:_:_:p) = head $ interpret (op:x:y:p)
+
+-- Part 2.
+
+interpretingYields :: Int -> [Int] -> Int -> Int -> Bool
+interpretingYields o p x y = o == interpretWithInputs x y p
+
+findInputs :: Int -> [Int] -> (Int, Int)
+findInputs o p = head $ filter (uncurry (interpretingYields o p)) [(x, y) | x <- [0..99], y <- [0..99]]
+
+solution :: Int -> [Int] -> Int
+solution o p = 100 * n + v
+    where (n, v) = findInputs o p
