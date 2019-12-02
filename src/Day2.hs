3264d67b001f08b09c4b8bb56297c8bbb38399e8
fwcd <fwcdmail@gmail.com>
2019-12-02 11:39:24 +0100

    Fix Intcode interpreter
    Make the interpreter operate on itself rather than modifying a separate
    memory tape.

--- src/Day2.hs
+++ src/Day2.hs
@@ -2,17 +2,20 @@
 
 replaceNth :: Int -> a -> [a] -> [a]
 replaceNth n z (x:xs) | n == 0 = z : xs
-                      | otherwise = replaceNth (n - 1) z xs
+                      | otherwise = x : replaceNth (n - 1) z xs
 
 -- Performs a single operation in Intcode.
-performOp :: [Int] -> [Int] -> ([Int], [Int])
-performOp (op:x:y:z:p) mem = (p, f mem)
-    where f = case op of
-                   0 -> replaceNth z (x + y)
-                   1 -> replaceNth z (x * y)
+performOp :: Int -> Int -> Int -> Int -> [Int] -> [Int]
+performOp op x y z m = case op of
+                          1 -> replaceNth z ((m !! x) + (m !! y)) m
+                          2 -> replaceNth z ((m !! x) * (m !! y)) m
+                          n -> error $ "Invalid op " <> show n
 
 -- Interprets a program written in Intcode.
-interpret :: [Int] -> [Int] -> [Int]
-interpret [] mem = mem
-interpret p mem = interpret remP newMem
-    where (remP, newMem) = performOp p mem
+interpret :: [Int] -> [Int]
+interpret m = interpret' m m
+    where interpret' :: [Int] -> [Int] -> [Int]
+          interpret' [] = id
+          interpret' (99:_) = id
+          interpret' (op:x:y:z:p) = interpret' p . performOp op x y z
+          interpret' p = const $ error $ "Parse error on remaining program: " <> show p
