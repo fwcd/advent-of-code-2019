c97ac69ed612b8cea7d6932346c69064575e4394
fwcd <fwcdmail@gmail.com>
2019-12-04 17:43:28 +0100

    Implement part 2 of day 4 (subsequence of max. length 2)

--- src/Day4.hs
+++ src/Day4.hs
@@ -3,10 +3,26 @@
 isPossiblyPassword :: Int -> Bool
 isPossiblyPassword i = adj i 10 False
     where adj :: Int -> Int -> Bool -> Bool
-          adj i d fnd | i == 0 = fnd || d == d'
-                      | otherwise = d >= d' && adj i' d' (fnd || d == d')
+          adj i d fnd | i == 0 = fnd'
+                      | otherwise = d >= d' && adj i' d' fnd'
             where i' = i `div` 10
                   d' = i `mod` 10
+                  fnd' = fnd || d == d'
 
 solution :: [Int] -> Int
 solution = length . filter isPossiblyPassword
+
+-- Part 2.
+
+isPossiblyPassword2 :: Int -> Bool
+isPossiblyPassword2 i = adj i 10 11 12 False
+    where adj :: Int -> Int -> Int -> Int -> Bool -> Bool
+          adj i d dd ddd fnd | i == 0 = fnd'
+                             | otherwise = d >= d' && adj i' d' d dd fnd'
+            where i' = i `div` 10
+                  d' = i `mod` 10
+                  fnd' = fnd || ((d' /= d) && (d == dd) && (dd /= ddd))
+
+solution2 :: [Int] -> Int
+solution2 = length . filter isPossiblyPassword2
+
