a6e72410fd499cbd0b7a5f7482c35db48853ddcf
fwcd <fwcdmail@gmail.com>
2019-12-08 20:30:52 +0100

    Day 6: Store orbits as in-memory graph structure

--- src/Day6.hs
+++ src/Day6.hs
@@ -5,23 +5,32 @@ import qualified Data.Map as M
 -- Part 1.
 
 data Orbit = Orbit String String deriving Show
+type OrbitMap = M.Map String [String]
 type OrbitCountMap = M.Map String Int
 
+-- Creates an in-memory graph structure from a list of orbits.
+buildOrbitMap :: [Orbit] -> OrbitMap
+buildOrbitMap = foldr insertOrbit M.empty
+    where insertOrbit :: Orbit -> OrbitMap -> OrbitMap
+          insertOrbit (Orbit x y) = M.insertWith (++) x [y]
+
 -- Finds the number of direct and indirect orbits for each planet.
-orbitCounts :: [Orbit] -> (OrbitCountMap, OrbitCountMap)
-orbitCounts os = let (ds, as) = orbitCounts' M.empty M.empty os
-                  in (ds, M.mapWithKey (\x y -> y - M.findWithDefault 0 x ds) as)
-    where orbitCounts' :: OrbitCountMap -> OrbitCountMap -> [Orbit] -> (OrbitCountMap, OrbitCountMap)
-          orbitCounts' ds as [] = (ds, as)
-          orbitCounts' ds as (Orbit x y:os) = orbitCounts' ds' as' os
-            where ds' = M.insertWith (+) y 1 ds
-                  as' = M.insertWith (+) y (1 + M.findWithDefault 0 x as) as
+-- orbitCounts:: [Orbit] -> (OrbitCountMap, OrbitCountMap)
+-- orbitCounts os = let (ds, as) = orbitCounts' M.empty M.empty os
+--                   in (ds, M.insert "COM" 0 $ M.mapWithKey (\x y -> y - M.findWithDefault 0 x ds) as)
+--     where orbitCounts' :: OrbitCountMap -> OrbitCountMap -> OrbitMap -> [Orbit] -> (OrbitCountMap, OrbitCountMap)
+--           orbitCounts' ds as _ [] = (ds, as)
+--           orbitCounts' ds as mos (Orbit x y:os) = orbitCounts' ds' as' mos' os
+--             where 
+--                   ds' = M.insertWith (+) y 1 ds
+--                   as' = updateAs o as
+--                   mos' = M.insertWith (++) x [y] mos
 
 -- Computes the sum of direct and indirect orbit counts.
-orbitCountSum :: [Orbit] -> Int
-orbitCountSum os = mapSum ds + mapSum is
-    where mapSum = M.foldr (+) 0
-          (ds, is) = orbitCounts os
+-- orbitCountSum :: [Orbit] -> Int
+-- orbitCountSum os = mapSum ds + mapSum is
+--     where mapSum = M.foldr (+) 0
+--           (ds, is) = orbitCounts os
 
 -- Splits a string around a character.
 splitOn :: Char -> String -> [String]
