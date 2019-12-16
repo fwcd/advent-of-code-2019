f6ff3844ad61b7853088bf6907588822decb1346
fwcd <fwcdmail@gmail.com>
2019-12-17 00:41:13 +0100

    Day 6: WIP: Reimplement commonOrbitedPlanet

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,3 +1,5 @@
+import Data.Functor
+import Data.Maybe
 import qualified Data.Map as M
 import qualified Data.List as L
 
@@ -44,17 +46,28 @@ parseOrbits = map parseOrbit
 
 -- Part 2.
 
-partitionUnique :: Eq a => [a] -> ([a], [a])
-partitionUnique = partitionUnique' [] []
-    where partitionUnique' :: Eq a => [a] -> [a] -> [a] -> ([a], [a])
-          partitionUnique' us ds [] = (us, ds)
-          partitionUnique' us ds (x:xs) = if elem x us then partitionUnique' (L.delete x us) (x:ds) xs
-                                                       else partitionUnique' (x:us) ds xs
+-- partitionUnique :: Eq a => [a] -> ([a], [a])
+-- partitionUnique = partitionUnique' [] []
+--     where partitionUnique' :: Eq a => [a] -> [a] -> [a] -> ([a], [a])
+--           partitionUnique' us ds [] = (us, ds)
+--           partitionUnique' us ds (x:xs) = if elem x us then partitionUnique' (L.delete x us) (x:ds) xs
+--                                                        else partitionUnique' (x:us) ds xs
+-- 
+-- safeMinimum :: Ord a => [a] -> Maybe a
+-- safeMinimum [] = Nothing
+-- safeMinimum xs = Just $ minimum xs
+
+expectJust :: String -> Maybe a -> a
+expectJust _ (Just x) = x
+expectJust msg Nothing = error msg
 
 commonOrbitedPlanet :: String -> String -> OrbitMap -> String
 commonOrbitedPlanet = commonOrbitedPlanet' "COM"
     where commonOrbitedPlanet' :: String -> String -> String -> OrbitMap -> String
-          commonOrbitedPlanet' z x y | z == x = undefined
-          distanceDownwardsBetween :: String -> String -> OrbitMap -> Int
-          distanceDownwardsBetween x y om | x == y = 0
-                                          | otherwise = foldr (+) 0 $ (\x' -> distanceDownwardsBetween x' y om) <$> M.findWithDefault [] x om
+          commonOrbitedPlanet' z x y om | z /= x && z /= y && xp == yp = commonOrbitedPlanet' xp x y om
+                                        | otherwise = z
+            where xp = expectJust ("No path from " <> z <> " to x = " <> x) $ step z x om
+                  yp = expectJust ("No path from " <> z <> " to y = " <> y) $ step z y om
+          step :: String -> String -> OrbitMap -> Maybe String
+          step x y om | x == y = Just ""
+                      | otherwise = listToMaybe $ (>>= \x' -> maybeToList $ x' <$ step x' y om) $ M.findWithDefault [] x om
