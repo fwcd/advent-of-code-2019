a1702454b36af1797369bc5cbbdbc7c52232bcb8
fwcd <fwcdmail@gmail.com>
2019-12-17 23:00:28 +0100

    Day 8: Finish part 2 by implementing image compositing

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,6 +1,9 @@
+import Control.Monad
 import Data.List
 
 -- Day 8: Space Image Format
+
+data Image = Image Int Int [Int]
 
 -- Part 1.
 
@@ -13,11 +16,29 @@ chunks n xs | null zs = [ys]
 count :: Eq a => a -> [a] -> Int
 count x = length . filter (== x)
 
-layers :: Int -> Int -> [a] -> [[a]]
-layers w h = chunks $ w * h
+layers :: Image -> [[Int]]
+layers (Image w h px) = chunks (w * h) px
 
 minimumComparing :: (a -> Int) -> [a] -> a
-minimumComparing f = minimumBy (\x y -> compare (f x) (f y))
+minimumComparing f = minimumBy $ \x y -> compare (f x) (f y)
 
-solution1 :: Int -> Int -> [Int] -> Int
-solution1 w h = (\l -> count 1 l * count 2 l) . minimumComparing (count 0) . layers w h
+solution1 :: Image -> Int
+solution1 = (\l -> count 1 l * count 2 l) . minimumComparing (count 0) . layers
+
+-- Part 2.
+
+composeLayers :: [Int] -> [Int] -> [Int]
+composeLayers xs [] = xs
+composeLayers [] ys = ys
+composeLayers xs ys = zipWith composer xs ys
+    where composer x y = if x == 2 then y
+                                   else x
+
+composeAllLayers :: Image -> [Int]
+composeAllLayers = foldl composeLayers [] . layers
+
+replace :: Eq a => a -> a -> [a] -> [a]
+replace o r = map $ \x -> if x == o then r else x
+
+printImage :: Image -> IO ()
+printImage img@(Image w _ _) = putStrLn $ unlines $ join <$> (chunks w $ replace "0" " " $ show <$> composeAllLayers img)
