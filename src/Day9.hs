c69a38e4d2a936f26e980ffe9b5e62bb5a538e11
fwcd <fwcdmail@gmail.com>
2019-12-18 00:20:36 +0100

    Day 9: Move performOp into State monad

--- src/Day9.hs
+++ src/Day9.hs
@@ -12,13 +12,13 @@ import qualified Data.Vector.Unboxed.Mutable as VUM
 
 type Memory = VU.Vector Int
 type InstructionPointerUpdate = Int -> Int
-data IntcodeMachine = IntcodeMachine { memory :: Memory, instPointer :: Int, relativeBase :: Int } deriving Show
+data IntcodeMachine = IntcodeMachine { memory :: Memory, instPtr :: Int, relativeBase :: Int } deriving Show
 
 -- A parameter mode determines how an operation's parameter should be turned into a value.
 type ParameterMode = Int -> Int -> Memory -> (Int, Int)
 
 immediateMode :: ParameterMode
-immediateMode x r m = (error "Immediate-mode parameters have no position", x)
+immediateMode x r m = (x, x)
 
 positionMode :: ParameterMode
 positionMode x r m = (x, m VU.! x) 
@@ -58,36 +58,73 @@ safeTail (x:xs) = xs
 -- Performs a single operation with automated inputs and outputs in Intcode.
 -- The first parameter is the opcode,
 -- the second parameter the input,
--- the third parameter the relative base,
--- the fourth parameter the memory starting at the parameter
--- and the fifth parameter the entire memory.
--- Returns the output, whether the input was used, the instruction pointer update function, the updated relative base and the new memory.
-performOp :: Int -> Maybe Int -> Int -> Memory -> Memory -> (Maybe Int, Bool, Int, InstructionPointerUpdate, Memory)
-performOp o i r p m = case op of
+-- the third parameter the memory starting at the parameter.
+-- Returns the output and whether the input was used.
+performOp :: Int -> Maybe Int -> Memory -> State IntcodeMachine (Maybe Int, Bool)
+performOp o i p = trace ("Performing op " <> show op <> " on " <> show p) $ case op of
                        -- Add
-                       1 -> noIO ((+3), replaceNth n ((pval 0) + (pval 1)) m)
-                          where n = ppos 2
+                       1 -> do
+                           x <- pval 0
+                           y <- pval 1
+                           n <- ppos 2
+                           memSet n $ x + y
+                           moveIP (+3)
+                           return noIO
                        -- Multiply
-                       2 -> noIO ((+3), replaceNth n ((pval 0) * (pval 1)) m)
-                          where n = ppos 2
+                       2 -> do
+                           x <- pval 0
+                           y <- pval 1
+                           n <- ppos 2
+                           m0 <- get
+                           memSet n $ x * y
+                           moveIP (+3)
+                           return noIO
                        -- Input
-                       3 -> (Nothing, True, r, (+1), replaceNth n inp m)
-                          where n = ppos 0
-                                inp = expectJust "Missing input during op 3!" i
+                       3 -> do
+                           n <- ppos 0
+                           let inp = expectJust "Missing input during op 3!" i
+                           memSet n inp
+                           moveIP (+1)
+                           return (Nothing, True)
                        -- Print
-                       4 -> (Just $ pval 0, False, r, (+1), m)
+                       4 -> do
+                           x <- pval 0
+                           moveIP (+1)
+                           return (Just x, False)
                        -- Jump-if-true
-                       5 -> noIO (if (pval 0) /= 0 then const $ pval 1 else (+2), m)
+                       5 -> do
+                           x <- pval 0
+                           y <- pval 1
+                           moveIP $ if x /= 0 then const y else (+2)
+                           return noIO
                        -- Jump-if-false
-                       6 -> noIO (if (pval 0) == 0 then const $ pval 1 else (+2), m)
+                       6 -> do
+                           x <- pval 0
+                           y <- pval 2
+                           moveIP $ if x == 0 then const y else (+2)
+                           return noIO
                        -- Less-than
-                       7 -> noIO ((+3), replaceNth n (intOfBool $ (pval 0) < (pval 1)) m)
-                          where n = ppos 2
+                       7 -> do
+                           x <- pval 0
+                           y <- pval 1
+                           n <- ppos 2
+                           memSet n $ intOfBool $ x < y
+                           moveIP (+3)
+                           return noIO
                        -- Equals
-                       8 -> noIO ((+3), replaceNth n (intOfBool $ (pval 0) == (pval 1)) m)
-                          where n = ppos 2
+                       8 -> do
+                           x <- pval 0
+                           y <- pval 1
+                           n <- ppos 2
+                           memSet n $ intOfBool $ x == y
+                           moveIP (+3)
+                           return noIO
                        -- Update relative base
-                       9 -> (Nothing, False, pval 0, (+1), m)
+                       9 -> do
+                           x <- pval 0
+                           moveBase $ const x
+                           moveIP (+1)
+                           return noIO
                        -- Unknown
                        n -> error $ "Invalid op " <> show n
     where (mode2, mode1, mode0, op) = parseOp o
@@ -95,14 +132,21 @@ performOp o i r p m = case op of
           mode 0 = mode0
           mode 1 = mode1
           mode 2 = mode2
-          param :: Int -> (Int, Int)
-          param x = mode x r (p VU.! x) m
-          pval :: Int -> Int
-          pval = snd . param
-          ppos :: Int -> Int
-          ppos = fst . param
-          noIO :: (a, b) -> (Maybe c, Bool, Int, a, b)
-          noIO (x, y) = (Nothing, False, r, x, y)
+          param :: Int -> State IntcodeMachine (Int, Int)
+          param x = do
+              mcn <- get
+              return $ mode x (p VU.! x) (relativeBase mcn) $ memory mcn
+          pval :: Int -> State IntcodeMachine Int
+          pval = (snd <$>) . param
+          ppos :: Int -> State IntcodeMachine Int
+          ppos = (fst <$>) . param
+          noIO = (Nothing, False)
+          moveIP :: InstructionPointerUpdate -> State IntcodeMachine ()
+          moveIP f = modify $ \mcn -> mcn { instPtr = f $ 1 + instPtr mcn }
+          moveBase :: (Int -> Int) -> State IntcodeMachine ()
+          moveBase f = modify $ \mcn -> mcn { relativeBase = f $ relativeBase mcn }
+          memSet :: Int -> Int -> State IntcodeMachine ()
+          memSet n x = modify $ \mcn -> mcn { memory = replaceNth n x $ memory mcn }
 
 -- Performs the next operation on the Intcode computer, possibly producing output and possibly halting.
 -- The resulting boolean determines whether the machine is "still running".
@@ -114,23 +158,17 @@ performNextOp = (\(o, c, _) -> (o, c)) <$> performNextOpWith Nothing
 performNextOpWith :: Maybe Int -> State IntcodeMachine (Maybe Int, Bool, Bool)
 performNextOpWith i = do
     mcn <- get
-    let ip = instPointer mcn
-        r = relativeBase mcn
-        m = memory mcn
     
     case nextInstruction mcn of
-        Just (op, p) | op /= 99 -> let (o, usedInput, r', ipUpdate, m') = performOp op i r p m
-                                       ip' = ipUpdate $ ip + 1
-                                       in do
-                                           put $ IntcodeMachine { memory = m', instPointer = ip', relativeBase = r' }
-                                           return (o, usedInput, True)
+        Just (op, p) | op /= 99 -> do (o, usedInput) <- performOp op i p
+                                      return (o, usedInput, True)
         _ -> return (Nothing, False, False)
 
 -- Fetches the next opcode and the subsequent memory cells.
 nextInstruction :: IntcodeMachine -> Maybe (Int, Memory)
 nextInstruction mcn | VU.null pp = Nothing
                     | otherwise = Just (VU.head pp, VU.tail pp)
-    where pp = VU.drop (instPointer mcn) (memory mcn)
+    where pp = VU.drop (instPtr mcn) (memory mcn)
 
 -- Performs the next operations on the Intcode computer using the given input sequence.
 -- Returns the outputs and whether the machine should continue.
@@ -148,7 +186,7 @@ performNextOpsWith is = do
 
 -- Creates a new machine with the given program loaded.
 newMachine :: [Int] -> IntcodeMachine
-newMachine pro = IntcodeMachine { memory = VU.fromList pro, instPointer = 0, relativeBase = 0 }
+newMachine pro = IntcodeMachine { memory = VU.fromList pro, instPtr = 0, relativeBase = 0 }
 
 -- Interprets a program written in Intcode with the given list of inputs, producing a list of outputs.
 interpretWith :: [Int] -> [Int] -> [Int]
